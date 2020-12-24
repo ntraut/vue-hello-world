19b828c2ef1a2e4436956e900ed0e6c2c8a5ce94
Nicolas <nicolas@example.com>
2020-12-24 15:24:05 +0100

    add vuex mutation

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="increment">X</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { mapMutations } from 'vuex'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+  method2() {
+    console.log('method2')
+  },
+  ...mapMutations(['increment'])
+}
 }
 </script>
 
